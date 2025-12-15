11ae1bc4ef11dd0dc4f6d514ee4bce5341d3c7b9
Michael <mluckau@gmail.com>
2025-12-15 23:46:56 +0100

    fix: localhost only

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ def add_header(response):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=5123)
+    app.run(debug=True, host='0.0.0.0', port=5123)
